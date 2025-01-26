c896fef21514f1b6116a063c1b63695d523901f2
espadv69 <aitorjob69@gmail.com>
2025-01-26 12:51:27 +0100

    Add initial player position to main.js

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 // DOM elements
 const $player = document.querySelector('.player')
 const $maze = document.querySelector('.maze')
+
+// Initial position of the player
+let playerPos = { x: 10, y: 10 }
